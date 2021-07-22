6ddab6cd5cf06dbe05229329a50857213064c3f2
Yusuke-Takahasi <yusuke.takahasi@gmail.com>
2021-07-22 22:38:07 +0900

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_application_1/TestPage1.dart';
+import 'package:flutter_application_1/TestPage2.dart';
+import 'package:flutter_application_1/TestPage3.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late PageController _pageController;
+  int _selectedIndex = 0;
+
+  //ボトムメニューの遷移先画面
+  var _pages = [
+    TestPage1(),
+    TestPage2(),
+    TestPage3(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _selectedIndex);
+  }
+
+  @override
+  void disposea() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return LoginPage();
+
+    return Scaffold(
+      body: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: _pages),
+    );
+  }
+}
